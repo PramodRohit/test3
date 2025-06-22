a15d31e391b32367e1a781d0fd064e37be4b8ec6
PramodRohit <129602240+PramodRohit@users.noreply.github.com>
2025-06-22 15:46:52 +0530

    Update Test3Application.java

--- src/main/java/com/test3/Test3Application.java
+++ src/main/java/com/test3/Test3Application.java
@@ -7,8 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test3Application {
 
 	public static void main(String[] args) {
-		int s = 100;
-		
+		int i = 100;
+		String a = "mike";
+		char r = 'q';
+		float w = 10.5f;
+		double s = 4.5;
+		int e = 200;
+		boolean f = false;
+		char ch = 'w';
+		long l = 10L;
 		SpringApplication.run(Test3Application.class, args);
 	}
 
